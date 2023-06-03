3470aedf122e5b19873090c3f50a3a34f41b395c
Mayank <7mayank.agarwal@gmail.com>
2023-06-03 23:46:18 +0530

    c

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 import { Sequelize } from 'sequelize'
 import * as dotenv from 'dotenv'
-import * as pg from 'pg'
 dotenv.config()
 
-console.log(process.env.POSTGRES_URL);
-const sequelize = new Sequelize(process.env.POSTGRES_URL,{
-  dialectModule: pg
+const sequelize = new Sequelize(process.env.dburi,{
+  logging: false
 });
 
 async function verifyConnection()
